447fa0e79dd27c4c4fdbff3aadf2e9570d35e078
adamk <adamkuczynski@gmail.com>
2021-05-31 19:39:13 +0200

    Zoom update

--- SimpleVerlet/Source.cpp
+++ SimpleVerlet/Source.cpp
@@ -9,10 +9,17 @@
 using sf::RenderWindow;
 using sf::Vector2f;
 
+const float zoomSpeed(0.02);
+
 int main()
 {
+    sf::View view(sf::FloatRect(0, 0, 800, 600));
     RenderWindow window(sf::VideoMode(800, 600), "Verlet");
     window.setFramerateLimit(60);
+    window.setView(view);
+    float viewZoom = 1;
+    bool panning = false;
+    Vector2f panStart;
 
     std::vector<Sphere> objectSet;
     int sizeObjectSet = 0;
@@ -25,15 +32,40 @@ int main()
     {
         sf::Time ElapsedTime = clock.getElapsedTime();
         sf::Time drawTime = clock.getElapsedTime();
-        if (ElapsedTime.asMilliseconds() > 500 && objectSet.size() < 4) {
+        if (ElapsedTime.asMilliseconds() > 500 && objectSet.size() < 15) {
             objectSet.push_back(Sphere(sizeObjectSet));
         }
 
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+
+            if (event.type == sf::Event::MouseWheelMoved) {
+                view.zoom(viewZoom - zoomSpeed * event.mouseWheel.delta);
+                window.setView(sf::View(view));
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                std::cout << "pressing" << std::endl;
+                panning = true;
+                panStart.x = sf::Mouse::getPosition(window).x;
+                panStart.y = sf::Mouse::getPosition(window).y;
+            }
+
+            if (panning == true) {
+                std::cout << "panning" << std::endl;
+                view.move(panStart.x - sf::Mouse::getPosition(window).x, panStart.y - sf::Mouse::getPosition(window).y);
+                window.setView(sf::View(view));
+                panStart.x = sf::Mouse::getPosition(window).x;
+                panStart.y = sf::Mouse::getPosition(window).y;
+            }
+
+            if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left) {
+                panning = false;
+            }
         }
 
         window.clear();
--- SimpleVerlet/Sphere.cpp
+++ SimpleVerlet/Sphere.cpp
@@ -8,6 +8,8 @@
 
 float Sphere::gravity = 2.0;
 float Sphere::elasticity = 0.50;
+float Sphere::thresh = 0.5;
+float Sphere::friction = 0.995;
 
 Sphere::Sphere(int sizeObjectSet) {
 	Sphere::id = sizeObjectSet;
@@ -41,57 +43,23 @@ void Sphere::update(sf::RenderWindow *window) {
 
 	// Floats denoting the distance to be covered in the x-direction (dx) and y-direction (dy).
 	float dx = position.x - oldPosition.x;
-	float dy = position.y - oldPosition.y;
-
-	sf::Vector2f gravVector(0.f, gravity);
-
-	sf::Vector2f gravPosition = position + gravVector;
-	sf::Vector2f gravOldPosition;
+	float dy = position.y - oldPosition.y + gravity;
 
 	// If the particle crosses a border, there are two useful distances to track: B shows how far beyond the border the particle has gone; A shows how much it covered before reaching the border.
 	float A, B;
 
 		if (position.x + dx > width) {
-			// In this case, the particle is running off the right side of the screen (exceeding its width).
-			// The distance to the border (A) is the width of the screen - the current position.x of the particle.
-			// E.g. if the width is 800, and the position.x is 750, then the particle is clearly 50 units away from the border.
 			A = width - position.x;
-
-			// The distance beyond the border is the expected position.x after movement (dx) minus the border.
-			// Hence, if the width is 800, the position.x is 750 and the dx is 150, then the particle is expected to move to (750 + 150) - 800 = 100 units beyond the border.
-			// These 100 units should not be covered, but instead be reversed in the opposite direction. Hence, B serves as an indication of how far the particle should bounce back (baseline without any dampening/forces)
 			B = position.x + dx - width;
-
-			// Need to move the oldPosition.x around, so that it will look like the particle came from the wall. This oldPosition.x should be the mirror image of the current position (prior to movement dx) along the wall that the particle is expected to strike.
-			// Since position.x is A units away from the border, if oldPosition.x is position.x plus 2*A, it will look as if oldPosition.x is a mirror image of position.x along the wall.
-			// If the width is 800, position.x is 750 and dx is 150, then it should seem as if the particle was coming from an x of 850, moving in the opposite direction by (A+B)=dx, where only the B part will remain visible.
 			oldPosition.x = position.x + 2 * A;
-
-			// Now, position.x itself can be updated. Since the particle still covers a distance of dx in total, it needs to move by A in one direction, and then B in the other direction.
-			// Can skip the movement in the A direction, since it'll immediately be undone, and change position.x in the opposite direction by B.
 			position.x = width - B;
-
-			// In this part, the particle is bouncing off the right wall, so that means it should end up at B units away from the border.
-			// If the width is 800, the position.x is 750 and dx is 150, then the particle should end up at an x of (800 - B) = 800 - 100 = 700.
-
-			// The particle should in the end only move somewhat to the left compared to its original position.
-			// If width is 800, position.x is 750 and dx is 150, then to move it to an x of 700, the particle needs to move (A - B) = (50 - 100) = -50.
-			// Negative amounts entail leftward movement, so this is all correct now.
 			dx = A - B;
 		}
 		else if (position.x + dx < 0) {
-			// Particle runs off the left side. Let position.x be 50 and dx is -150, then the particle would end up at x of -100. It should end up at +100 instead.
-			// Distance to the border (A) is 0 - position.x = -position.x. Negative number implies it's to the left.
 			A = -position.x;
-
-			// Distance it would cover beyond the border is dx - A. With the prior numbers, (dx - A) = (-150 -(-50)) = -100
 			B = dx - A;
 
-			// Old position needs to be updated first again. For future reference, need to pretend that the particle was coming from the left all along.
-			// With above numbers, it should look as if it's coming from a position of -50, so that when the particle is done traveling a dx of 150 it ends up at +100.
 			oldPosition.x = position.x + 2 * A;
-
-			// With prior numbers, particle should move 50 left and 100 right, so 50 right overall. Position should then be (A - B) = (-50 -(-100)) = 50
 			position.x = -B;
 
 			dx = A - B;
@@ -102,25 +70,17 @@ void Sphere::update(sf::RenderWindow *window) {
 		}
 
 		if (position.y + dy > height) {
-			// Particle is running off the bottom of the screen. Logic is the same as when it runs off the right.
-			// If the height is 600, and the position.y is 550, then the particle is 50 units away from the border.
 			A = height - position.y;
-
-			// Let height be 600, position.y be 550 and dy be 150. Then the particle will travel (550 + 150) - 600 = 100 units too far.
-			B = position.y + dy - height;
+			B = position.y + dy - height - gravity;
 			
-			// Update old position vector to make it a mirror image again.
 			oldPosition.y = position.y + 2 * A;
-
-			// New position should be based on the distance traveled in reverse from the border, so height - B. With the example numbers here that's 600 - 100 = 500
 			position.y = height - B;
 
-			// Actual distance traveled is here A - B = 50 - 100 = -50, so that position.y + dy = 500.
 			dy = A - B;
 		}
 		else if (position.y + dy < 0) {
 			A = -position.y;
-			B = dy - A;
+			B = dy - A + gravity;
 
 			oldPosition.y = position.y + 2 * A;
 			position.y = -B;
@@ -130,6 +90,14 @@ void Sphere::update(sf::RenderWindow *window) {
 		else {
 			oldPosition.y = position.y;
 			position.y += dy;
+		}
+
+		if (position.y > height - thresh && oldPosition.y > height - thresh && dy < thresh) {
+			dy = 0;
+			position.y = height;
+			oldPosition.y = height;
+			position.x = position.x + (1 - friction )* (oldPosition.x - position.x);
+			dx *= friction;
 		}
 
 	shape.move(dx, dy);
